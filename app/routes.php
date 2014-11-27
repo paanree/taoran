a31497fb50667cec7e44ed5bb29a8ce1e5cd2d74
ryne_pang <ryne_pang@adsame.com>
2014-11-27 11:42:11 +0800

    ryne
    Initialization

---
+++ app/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+
+Route::get('users', function()
+{   
+     $timezone = Config::get('app.timezone', '135246');
+	echo $timezone;
+	echo 123;
+
+    $users = User::all();
+
+    return View::make('users')->with('users', $users);
+});
